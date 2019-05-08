e34cf67656bc2e803c3d74b4734b0d1b98abba41
Your Name <karolina.kantorska@gmail.com>
2019-05-08 12:04:48 +0200

    corrections after first review

--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ createRestaurantHTML = (restaurant) => {
   // KK
   image.alt = restaurant.name;
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
